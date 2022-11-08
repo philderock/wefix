eb4a7a0c2b194d30ffbde01c07ba188289b37f75
Philip Chukwuma <cchukwuma.p@gmail.com>
2022-11-08 11:27:59 +0000

    Splash screen added and completed

--- WeFix/app/src/main/java/uk/ac/tees/b1498820/wefix/MainActivity.java
+++ WeFix/app/src/main/java/uk/ac/tees/b1498820/wefix/MainActivity.java
@@ -2,7 +2,9 @@ package uk.ac.tees.b1498820.wefix;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // set handler to run task for specific time interval
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                //create the intent
+                Intent splashScreenIntent = new Intent(MainActivity.this, LoginActivity.class);
+
+                //Start activity
+                startActivity(splashScreenIntent);
+                finish();
+            }
+        }, 2000);
+
     }
 }
