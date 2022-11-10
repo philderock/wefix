58d08dafbab001e05304ac11c9468eee7dfb2991
Philip Chukwuma <cchukwuma.p@gmail.com>
2022-11-10 17:18:12 +0000

    Staff Sign up form added

---
+++ WeFix/app/src/main/java/uk/ac/tees/b1498820/wefix/StaffSignupActivity.java
@@ -0,0 +1,14 @@
+package uk.ac.tees.b1498820.wefix;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class StaffSignupActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_staff_signup);
+    }
+}
